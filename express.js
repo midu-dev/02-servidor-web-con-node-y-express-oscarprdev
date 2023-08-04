e2473d9f88357d8af1cd73f13b73d6fc9e4a1e3a
vlcoscar8 <vlcoscar8@github.com>
2023-08-04 12:28:33 +0200

    Ejercicio 02. Servidor web con Express

--- express.js
+++ express.js
@@ -1,7 +1,53 @@
 // Ejercicio 2: crear servidor HTTP con Express
+const fs = require('node:fs/promises')
+const express = require('express')
+const ROUTES = require('./routes')
+
+const PORT = process.env.PORT ?? 1234
+
+const app = express()
+app.use(express.json())
+
+async function retrieveLogo(res) {
+  try {
+    const data = await fs.readFile('./assets/logo.webp')
+
+    return res.set('content-type', 'image/webp').end(data)
+  } catch (e) {
+    return res.status(501).send('<h1>500 Internal Server Error</h1>')
+  }
+}
+
+function retrieveContact(req, res) {
+  const { body } = req
+
+  if (Object.keys(body).some(key => !['name', 'email', 'message'].includes(key))) {
+    return res.status(400).send('<h1>Invalid request body</h1>')
+  }
+
+  return res.status(201).send(body)
+}
+
+function isMethodNotAllowed(req) {
+  const {url} = req
+
+  return Object.values(ROUTES).some(route => route === url)
+}
+
+app.get(ROUTES.INITIAL, (req, res) => res.send('<h1>¡Hola mundo!</h1>'))
+
+app.get(ROUTES.LOGO, (req, res) => retrieveLogo(res))
+
+app.post(ROUTES.CONTACTO, (req, res) => retrieveContact(req, res))
+
+app.use((req, res) => {
+  return isMethodNotAllowed(req)
+    ? res.status(405).send('<h1>Method Not Allowed</h1>')
+    : res.status(404).send('<h1>404</h1>')
+})
+
 function startServer () {
-  // const server = app.listen(...)
-  // return server
+  app.listen(PORT, () => console.log(`server listening on port http://localhost:${PORT}`))
 }
 
 module.exports = {
